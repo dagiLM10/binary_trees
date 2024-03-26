02b922f93e7efa617587247f5f8edd8021b7541c
dagiLM10 <dagiberhanu19@gmail.com>
2024-03-26 06:59:55 -0700

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((1 > r) ? l : r);
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
